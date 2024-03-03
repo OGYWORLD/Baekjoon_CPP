56a5ca9bceaca409297ae36f4c25760fcebad63e
OZY <ogy1004@naver.com>
2024-03-03 21:05:28 +0900

    15666

---
+++ 백트래킹/15666.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+static vector<int> Adj;
+static vector<bool> IsVisited;
+static vector<int> PrintVector;
+
+void BackTracking(int depth, int& M, int& N, int BeforeDepthNode)
+{
+	if (depth == M)
+	{
+		for (int i = 0; i < M; i++)
+		{
+			cout << PrintVector[i] << " ";
+		}
+
+		cout << "\n";
+	}
+	else
+	{
+		int BeforeNode = -1;
+
+		for (int i = 0; i < N; i++)
+		{
+			if (Adj[i] != BeforeNode && BeforeDepthNode <= Adj[i])
+			{
+				PrintVector[depth] = Adj[i];
+				BeforeNode = Adj[i];
+				BackTracking(depth + 1, M, N, Adj[i]);
+			}
+		}
+	}
+
+}
+
+int main()
+{
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int N, M;
+	cin >> N >> M;
+
+	IsVisited.resize(N, false);
+	Adj.resize(N);
+	PrintVector.resize(N);
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> Adj[i];
+	}
+
+	sort(Adj.begin(), Adj.end());
+
+	BackTracking(0, M, N, -1);
+
+	
+
+	return 0;
+}
