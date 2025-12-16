3d23d3f4e13349fecee3cb4ce3c9c355bad5d502
GaeulOh <ogy1004@naver.com>
2025-12-16 23:42:29 +0900

    1402

---
+++ 수학/1402.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int t;
+    cin >> t;
+
+    int a, b;
+    for (int i = 0; i < t; i++)
+    {
+        cin >> a >> b;
+        cout << "yes\n";
+    }
+
+    return 0;
+}
